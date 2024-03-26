08a3cdd138f701914e6320ed2284f170c2d9c5f6
Abdullah Karam <abdallakaramdev@gmail.com>
2024-03-26 19:43:57 +0200

    Failure Output

---
+++ tests/FailureOutput/ArrayWeakComparisonTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace FailureOutput;
+
+use PHPUnit\Framework\TestCase;
+
+final class ArrayWeakComparisonTest extends TestCase
+{
+    public function testEquality(): void
+    {
+        $this->assertEquals(
+            [1, 2, 3, 4, 5, 6],
+            [1, 2, 3, 4, 5, 6],
+        );
+    }
+}
